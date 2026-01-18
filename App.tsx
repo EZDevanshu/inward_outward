2f8a5b536b1b5000fc81d268d88fbd1efb9a3ce1
EZDevanshu <devanshuparmar00@gmail.com>
2026-01-18 21:54:17 +0530

    changed

--- App.tsx
+++ App.tsx
@@ -37,7 +37,7 @@ const App: React.FC = () => {
               <Route path="/entry" element={<EntryForm />} />
               <Route path="/entry/inward" element={<EntryForm initialType={EntryType.INWARD} />} />
               <Route path="/entry/outward" element={<EntryForm initialType={EntryType.OUTWARD} />} />
-              
+        
               <Route path="/registers/inward" element={<Register type={EntryType.INWARD} />} />
               <Route path="/registers/outward" element={<Register type={EntryType.OUTWARD} />} />
               
